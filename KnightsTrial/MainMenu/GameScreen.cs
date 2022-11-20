528b10c5f76b130f2f686ecce588c01fb13a91ef
BrokenModem <larsespenhain@hotmail.com>
2022-11-20 21:14:06 +0100

    Small fixes.

--- KnightsTrial/MainMenu/GameScreen.cs
+++ KnightsTrial/MainMenu/GameScreen.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KnightsTrial.MainMenu
+namespace KnightsTrial
 {
     internal class GameScreen : MainMenu
     {
--- KnightsTrial/MainMenu/MainMenu.cs
+++ KnightsTrial/MainMenu/MainMenu.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KnightsTrial.MainMenu
+namespace KnightsTrial
 {
     internal class MainMenu
     {
