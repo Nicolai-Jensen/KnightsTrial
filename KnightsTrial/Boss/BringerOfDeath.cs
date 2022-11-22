f7bd41b876157bebde793d7904a197dcc709a5e8
BigMcFatHuge <97097221+BigMcFatHuge@users.noreply.github.com>
2022-11-22 21:16:54 +0100

    BoD MovementBehaviour
    MovementBehaviour improved.

--- KnightsTrial/Boss/BringerOfDeath.cs
+++ KnightsTrial/Boss/BringerOfDeath.cs
@@ -14,6 +14,11 @@ namespace KnightsTrial
     {
         //Fields
         private Vector2 playerPosition;
+        private float behaviourTimer;
+        private Random rndBehaviour = new Random();
+        private int randomTimeCount;
+        private bool animationReset;
+
         //Properties
 
         //Constructors
@@ -21,29 +26,39 @@ namespace KnightsTrial
         {
             GameState.InstantiateGameObject(this);
             health = 2500;
-            speed = 150f;
+            speed = 120f;
             velocity = new Vector2(1, 0);
             position = new Vector2(200, 200);
+            randomTimeCount = 3;
+            animationReset = true;
 
-            objectSprites = new Texture2D[8];
+            walkAnimation = new Texture2D[8];
+            magicAnimation = new Texture2D[9];
         }
 
         //Methods
         public override void LoadContent(ContentManager content)
         {
-            for (int i = 0; i < objectSprites.Length; i++)
+            for (int i = 0; i < walkAnimation.Length; i++)
             {
-                objectSprites[i] = content.Load<Texture2D>($"BringerOfDeath/Bosswalk_{i}");
+                walkAnimation[i] = content.Load<Texture2D>($"BringerOfDeath/Bosswalk_{i}");
             }
+            for (int i = 0; i < magicAnimation.Length; i++)
+            {
+                magicAnimation[i] = content.Load<Texture2D>($"BringerOfDeath/Bringer-of-Death_Cast_{i+1}");
+            }
+
+            objectSprites = walkAnimation;
         }
 
         public override void Update(GameTime gameTime)
         {
             playerPosition = GetPlayer().Position;
+            origin = new Vector2(objectSprites[0].Width / 2, objectSprites[0].Height);
 
+            MovementBehaviour(gameTime);
             Move(gameTime);
             Animate(gameTime);
-            FollowPlayer();
         }
 
         public override void Draw(SpriteBatch spriteBatch)
@@ -77,6 +92,36 @@ namespace KnightsTrial
             return null;
         }
 
+        private void MovementBehaviour(GameTime gameTime)
+        {
+            behaviourTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (behaviourTimer < randomTimeCount)
+            {
+                objectSprites = walkAnimation;
+                origin = new Vector2(objectSprites[0].Width / 2, objectSprites[0].Height);
+                FollowPlayer();
+            }
+            else if (behaviourTimer > randomTimeCount && behaviourTimer < randomTimeCount + 1)
+            {
+                if (animationReset)
+                {
+                    animationTime = 0;
+                    animationReset = false;
+                }
+
+                objectSprites = magicAnimation;
+                origin = new Vector2(objectSprites[0].Width / 2, objectSprites[0].Height);
+                velocity = new(0, 0);
+            }
+            else if (behaviourTimer > randomTimeCount + 1)
+            {
+                randomTimeCount = rndBehaviour.Next(1, 6);
+                behaviourTimer = 0;
+                animationReset = true;
+            }
+        }
+
         private void FollowPlayer()
         {
             Vector2 outputVelocity = playerPosition - position;
@@ -99,6 +144,10 @@ namespace KnightsTrial
         public void AOEAttack(GameTime gameTime)
         {
             // Area of effect attack.
+        }
+        public void PhaseShiftAttack(GameTime gameTime)
+        {
+
         }
     }
 }
