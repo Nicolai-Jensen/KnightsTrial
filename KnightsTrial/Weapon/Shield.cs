c719103a1c15e0aafa0d5e0e05baa9e89ba9b366
Nicolai-Jensen <nicolaijensen11@gmail.com>
2022-11-29 12:52:47 +0100

    Kommenteret
    I Player
    I Weapon
    og I Shield
    har også fjernede kode som ikke blev brugt

--- KnightsTrial/Weapon/Shield.cs
+++ KnightsTrial/Weapon/Shield.cs
@@ -14,8 +14,7 @@ namespace KnightsTrial
     {
         //Fields
 
-        private int blockValue;
-
+        //Variables used for the mouse to later avtivate single presses/releases
         private MouseState currentMouse;
         private MouseState previousMouse;
 
@@ -23,24 +22,37 @@ namespace KnightsTrial
 
         //Constructors
 
+        /// <summary>
+        /// The Constructor for shield
+        /// </summary>
+        /// <param name="sprite">The Texture that the shield uses</param>
+        /// <param name="position">The position of the shield</param>
         public Shield(Texture2D sprite, Vector2 position)
         {
             objectSprites = new Texture2D[1];
             objectSprites[0] = sprite;
             scale = 0.1f;
-
             this.position = position;
         }
 
         //Methods
 
+        /// <summary>
+        /// This Method loads in information and code needed for the game, there is nothing in this one but it is needed anyway as Gameobject has it abstract
+        /// </summary>
+        /// <param name="content">Parameter given by the monogame framework for our use</param>
         public override void LoadContent(ContentManager content)
         {
             origin = new Vector2(objectSprites[0].Width / 2, objectSprites[0].Height / 2);
         }
 
+        /// <summary>
+        /// This Update Method constantly loops throughout the program aslong as it is running, other methods we want to be looped are called inside this one
+        /// </summary>
+        /// <param name="gameTime">Parameter given by the Monogame framework to simulate time</param>
         public override void Update(GameTime gameTime)
         {
+            //constantly updates the state of the mouse buttons 
             previousMouse = currentMouse;
             currentMouse = Mouse.GetState();
 
@@ -48,11 +60,19 @@ namespace KnightsTrial
             RemoveShield();
         }
 
+        /// <summary>
+        /// The Method for Drawing out a sprite to the screen, this method is an override for the vitual one in GameObject and is called in GameWorld/GameState
+        /// </summary>
+        /// <param name="spriteBatch">Parameter given by the Monogame framework for the use of sprites</param>
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(objectSprites[(int)animationTime], position, null, Color.Aqua * 0.4f, 0, origin, scale, SpriteEffects.None, 1f);
         }
 
+        /// <summary>
+        /// This Method is played as long as the right mouse buttons is held down, it keeps the blocking bool on the player active
+        /// </summary>
+        /// <param name="gameTime">Parameter given by the Monogame framework to simulate time</param>
         public void HoldBlock(GameTime gameTime)
         {
             MouseState mouseState = Mouse.GetState();
@@ -66,6 +86,9 @@ namespace KnightsTrial
             }
         }
 
+        /// <summary>
+        /// This Method is used to remove the shield and reactive the players movement/stamina
+        /// </summary>
         public void RemoveShield()
         {
             if (currentMouse.RightButton == ButtonState.Released && previousMouse.RightButton == ButtonState.Pressed)
--- KnightsTrial/Weapon/Weapon.cs
+++ KnightsTrial/Weapon/Weapon.cs
@@ -13,9 +13,8 @@ namespace KnightsTrial
     {
         //Fields
 
+        //The field values used by all weapons are protected here
         protected float rotation;
-        protected bool enoughStamina;
-        protected bool useArsenalAnimation;
         protected static int damageValue;
 
         //Properties
@@ -32,24 +31,31 @@ namespace KnightsTrial
         }
 
         //Constructors
-
+        //There is no constructor for this superclass
 
         //Methods
+
+        /// <summary>
+        /// This Method loads in information and code needed for the game, there is nothing in this one but it is needed anyway as Gameobject has it abstract
+        /// </summary>
+        /// <param name="content">Parameter given by the monogame framework for our use</param>
         public override void LoadContent(ContentManager content)
         {
-            origin = new Vector2(objectSprites[0].Width / 2, objectSprites[0].Height / 2);
+            
         }
 
+        /// <summary>
+        /// This Update Method constantly loops throughout the program aslong as it is running, other methods we want to be looped are called inside this one
+        /// </summary>
+        /// <param name="gameTime">Parameter given by the Monogame framework to simulate time</param>
         public override void Update(GameTime gameTime)
         {
-            Move(gameTime);
         }
 
-        protected void UseArsenal(GameTime gameTime)
-        {
-
-        }
-
+        /// <summary>
+        /// Method used by all weapoons to find the position of the PLayer
+        /// </summary>
+        /// <returns></returns>
         protected Vector2 ReturnPlayerPostition()
         {
             foreach (GameObject go in GameState.gameObject)
@@ -65,6 +71,10 @@ namespace KnightsTrial
             return new Vector2(position.X, -100);
         }
 
+        /// <summary>
+        /// Method used to set the animationtime (place in the array) of the player before swaping player animations, this is used because otherwise swapping arrays from an array with 5 spot to 2 spots may result in Null
+        /// </summary>
+        /// <param name="timeValue">timeValue in this method is the spot in the array or the players "animationTime" variable</param>
         protected void SetPlayerAnimationTime(float timeValue)
         {
             foreach (GameObject go in GameState.gameObject)
@@ -76,6 +86,10 @@ namespace KnightsTrial
             }
         }
 
+        /// <summary>
+        /// Method used to spend stamina when attacking
+        /// </summary>
+        /// <param name="value">Value is the amount of stamina that is taken from the player</param>
         protected void SetPlayerStamina(int value)
         {
 
@@ -83,6 +97,10 @@ namespace KnightsTrial
           
         }
 
+        /// <summary>
+        /// Method used to change the players speed value as when attacking he needs to commit to his positioning
+        /// </summary>
+        /// <param name="speedValue">speedValue is the value the players speed is set too</param>
         protected void SetPlayerSpeed(float speedValue)
         {
             foreach (GameObject go in GameState.gameObject)
@@ -94,12 +112,19 @@ namespace KnightsTrial
             }          
         }
 
+        /// <summary>
+        /// This Method is used to determine the direction of the weapon objects
+        /// </summary>
+        /// <param name="playerPosition">While it can be any position, we called it playerPosition as inserting "ReturnPlayerPosition" method giver den resultat vi vil have</param>
+        /// <returns></returns>
         protected Vector2 Direction(Vector2 playerPosition)
         {
             Vector2 direction;
+            //Usses a static class we made to convert Points to vector2s
             MouseState mouseState = Mouse.GetState();
             Vector2 mousePosition = mouseState.Position.ToVector2();
 
+            //We get the correct direction by Subtracting the player Position with the Mouse Point, same goes for Rotation
             direction = mousePosition - playerPosition;
             direction.Normalize();
             rotation = (float)Math.Atan2(mousePosition.Y - position.Y, mousePosition.X - position.X) - 0.1f;
@@ -107,11 +132,19 @@ namespace KnightsTrial
             return direction;
         }
 
+        /// <summary>
+        /// The OnCollision method is used for collision if it has to react in anyway, however most of our collision is done in the bosses collision
+        /// </summary>
+        /// <param name="other">other is the object colliding with this object</param>
         public override void OnCollision(GameObject other)
         {
 
         }
 
+        /// <summary>
+        /// This Method gets the player and lets you use the players properties as long as they are public
+        /// </summary>
+        /// <returns></returns>
         protected Player GetPlayer()
         {
             //loops through the gameObject list untill it finds the player, then returns it. 
